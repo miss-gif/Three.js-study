90a9c6585c9a4d10cc266def1e2514f8128e99d2
miss-gif <svx327@naver.com>
2024-12-18 18:01:38 +0900

    Test: three를 사용한 3D 랜더링

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
-# npm 소프트 프로젝트 템플릿
+## 설치
 
-- Vite
-- TypeScript
-- tailwindcss
-- shancn/ui
-- axios
-- @tanstack/react-query
-- react-router-dom
-- react-hook-form
-- zod
-- @hookform/resolvers
-- eslint
-- prettier
+- three
+- @react-three/fiber
+- @react-three/drei
+
+## glb의 모델을 컴포넌트화 해주는 툴
+
+- 파일이 있는 위치에서 실행해야 함
+
+- npx gltfjsx avata.glb
+
+##
+
+- https://readyplayer.me/
+- https://fund.blender.org/
+- https://www.mixamo.com/
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { ModelAni } from './components/Model-ani';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="w-full h-screen">
+      <Canvas
+        camera={{ position: [2, 0, 12.25], fov: 15 }}
+        className="w-full h-dvh bg-neutral-800"
+      >
+        <ambientLight intensity={1.25} />
+        <ambientLight intensity={0.1} />
+        <directionalLight intensity={0.5} />
+        <ModelAni position={[0.025, -0.9, 0]} />
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
 };
 
 export default App;
